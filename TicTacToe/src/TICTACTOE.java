2f678559606ecd84d4fc5a9c01954fe883adff0b
Luca <Luca@Lucas-PC.fritz.box>
2015-04-04 14:52:41 +0200

    Fertige Version

--- TicTacToe/src/TICTACTOE.java
+++ TicTacToe/src/TICTACTOE.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*;
 
 import java.awt.*;
@@ -57,9 +56,9 @@ public class TICTACTOE extends JFrame implements ActionListener {
 		JPanel x = new JPanel();
 		x.setLayout(new GridLayout(3, 3));
 
-		this.setTitle("TIC TAC TOE  --  Spieler 1 beginnt");
+		this.setTitle("TIC TAC TOE  --  Spieler O beginnt");
 		this.setSize(800, 800);
-		this.setLocation(300,100);
+		this.setLocation(300, 100);
 		this.add(x);
 		this.setVisible(true);
 		x.add(button1);
@@ -112,7 +111,7 @@ public class TICTACTOE extends JFrame implements ActionListener {
 		x8 = null;
 		x9 = null;
 
-		spieler = 0;
+		spieler = 1;
 
 		button1.setText(null);
 		button2.setText(null);
@@ -127,115 +126,131 @@ public class TICTACTOE extends JFrame implements ActionListener {
 	}
 
 	public void gewinnerTest() {
-		if (spieler % 9 == 0) {
-			this.setTitle("Unentschieden -- Spieler 1 ist an der Reihe!");
+		if (spieler % 11 == 0) {
+			this.setTitle("Unentschieden -- Spieler X ist an der Reihe!");
+			
 			clearButton();
+		} else {
+
+			if (x1 != null && x2 != null && x3 != null) {
+
+				if (x1.equals(x2) && x2.equals(x3) && x1.equals(x3)) {
+					if (x1.contains("O")) {
+						this.setTitle("Spieler O hat gewonnen! Spieler X ist an der Reihe");
+						clearButton();
+						spieler = 1;
+					}
+					if (x1.contains("X")) {
+						this.setTitle("Spieler X hat gewonnen! Spieler O ist an der Reihe");
+						clearButton();
+						spieler = 2;
+					}
+				}
+			}
+
+			if (x1 != null && x4 != null && x7 != null) {
+
+				if (x1.equals(x4) && x4.equals(x7)) {
+					if (x1.contains("O")) {
+						this.setTitle("Spieler O hat gewonnen! Spieler X ist an der Reihe");
+						clearButton();
+						spieler = 1;
+					}
+					if (x1.contains("X")) {
+						this.setTitle("Spieler X hat gewonnen! Spieler O ist an der Reihe");
+						clearButton();
+						spieler = 2;
+					}
+
+				}
+			}
+
+			if (x1 != null && x5 != null && x9 != null) {
+				if (x1.equals(x5) && x5.equals(x9)) {
+					if (x1.contains("O")) {
+						this.setTitle("Spieler O hat gewonnen! Spieler X ist an der Reihe");
+						clearButton();
+						spieler = 1;
+					}
+					if (x1.contains("X")) {
+						this.setTitle("Spieler X hat gewonnen! Spieler O ist an der Reihe");
+						clearButton();
+						spieler = 2;
+					}
+				}
+			}
+			if (x2 != null && x5 != null && x8 != null) {
+				if (x2.equals(x5) && x5.equals(x8)) {
+					if (x2.contains("O")) {
+						this.setTitle("Spieler O hat gewonnen! Spieler X ist an der Reihe");
+						clearButton();
+						spieler = 1;
+					}
+					if (x2.contains("X")) {
+						this.setTitle("Spieler X hat gewonnen! Spieler O ist an der Reihe");
+						clearButton();
+						spieler = 2;
+					}
+				}
+			}
+			if (x3 != null && x6 != null && x9 != null) {
+				if (x3.equals(x6) && x6.equals(x9)) {
+					if (x3.contains("O")) {
+						this.setTitle("Spieler O hat gewonnen! Spieler X ist an der Reihe");
+						clearButton();
+						spieler = 1;
+					}
+					if (x3.contains("X")) {
+						this.setTitle("Spieler X hat gewonnen! Spieler O ist an der Reihe");
+						clearButton();
+						spieler = 2;
+					}
+				}
+			}
+			if (x4 != null && x5 != null && x6 != null) {
+				if (x4.equals(x5) && x5.equals(x6)) {
+					if (x4.contains("O")) {
+						this.setTitle("Spieler O hat gewonnen! Spieler X ist an der Reihe");
+						clearButton();
+						spieler = 1;
+					}
+					if (x4.contains("X")) {
+						this.setTitle("Spieler X hat gewonnen! Spieler O ist an der Reihe");
+						clearButton();
+						spieler = 2;
+					}
+				}
+			}
+			if (x7 != null && x7 != null && x9 != null) {
+				if (x7.equals(x8) && x8.equals(x9)) {
+					if (x7.contains("O")) {
+						this.setTitle("Spieler O hat gewonnen! Spieler X ist an der Reihe");
+						clearButton();
+						spieler = 1;
+					}
+					if (x7.contains("X")) {
+						this.setTitle("Spieler X hat gewonnen! Spieler O ist an der Reihe");
+						clearButton();
+						spieler = 2;
+					}
+				}
+			}
+			if (x3 != null && x5 != null && x7 != null) {
+				if (x3.equals(x5) && x5.equals(x7)) {
+					if (x3.contains("O")) {
+						this.setTitle("Spieler O hat gewonnen! Spieler X ist an der Reihe");
+						clearButton();
+						spieler = 1;
+					}
+					if (x3.contains("X")) {
+						this.setTitle("Spieler X hat gewonnen! Spieler O ist an der Reihe");
+						clearButton();
+						spieler = 2;
+					}
+				}
+			}
 		}
 
-		if (x1 != null && x2 != null && x3 != null) {
-
-			if (x1.equals(x2) && x2.equals(x3) && x1.equals(x3)) {
-				if (x1.contains("O")) {
-					this.setTitle("Spieler 2 hat gewonnen! Spieler 1 ist an der Reihe");
-					clearButton();
-				}
-				if (x1.contains("X")) {
-					this.setTitle("Spieler 1 hat gewonnen! Spieler 2 ist an der Reihe");
-					clearButton();
-				}
-			}
-		}
-
-		if (x1 != null && x4 != null && x7 != null) {
-
-			if (x1.equals(x4) && x4.equals(x7)) {
-				if (x1.contains("O")) {
-					this.setTitle("Spieler 2 hat gewonnen! Spieler 1 ist an der Reihe");
-					clearButton();
-				}
-				if (x1.contains("X")) {
-					this.setTitle("Spieler 1 hat gewonnen! Spieler 2 ist an der Reihe");
-					clearButton();
-				}
-
-			}
-		}
-
-		if (x1 != null && x5 != null && x9 != null) {
-			if (x1.equals(x5) && x5.equals(x9)) {
-				if (x1.contains("O")) {
-					this.setTitle("Spieler 2 hat gewonnen! Spieler 1 ist an der Reihe");
-					clearButton();
-				}
-				if (x1.contains("X")) {
-					this.setTitle("Spieler 1 hat gewonnen! Spieler 2 ist an der Reihe");
-					clearButton();
-				}
-			}
-		}
-		if (x2 != null && x5 != null && x8 != null) {
-			if (x2.equals(x5) && x5.equals(x8)) {
-				if (x2.contains("O")) {
-					this.setTitle("Spieler 2 hat gewonnen! Spieler 1 ist an der Reihe");
-					clearButton();
-				}
-				if (x2.contains("X")) {
-					this.setTitle("Spieler 1 hat gewonnen! Spieler 2 ist an der Reihe");
-					clearButton();
-				}
-			}
-		}
-		if (x3 != null && x6 != null && x9 != null) {
-			if (x3.equals(x6) && x6.equals(x9)) {
-				if (x3.contains("O")) {
-					this.setTitle("Spieler 2 hat gewonnen! Spieler 1 ist an der Reihe");
-					clearButton();
-				}
-				if (x3.contains("X")) {
-					this.setTitle("Spieler 1 hat gewonnen! Spieler 2 ist an der Reihe");
-					clearButton();
-				}
-			}
-		}
-		if (x4 != null && x5 != null && x6 != null) {
-			if (x4.equals(x5) && x5.equals(x6)) {
-				if (x4.contains("O")) {
-					this.setTitle("Spieler 2 hat gewonnen! Spieler 1 ist an der Reihe");
-					clearButton();
-				}
-				if (x4.contains("X")) {
-					this.setTitle("Spieler 1 hat gewonnen! Spieler 2 ist an der Reihe");
-					clearButton();
-				}
-			}
-		}
-		if (x7 != null && x7 != null && x9 != null) {
-			if (x7.equals(x8) && x8.equals(x9)) {
-				if (x7.contains("O")) {
-					this.setTitle("Spieler 2 hat gewonnen! Spieler 1 ist an der Reihe");
-					clearButton();
-				}
-				if (x7.contains("X")) {
-					this.setTitle("Spieler 1 hat gewonnen! Spieler 2 ist an der Reihe");
-					clearButton();
-				}
-			}
-		}
-		if (x3 != null && x5 != null && x7 != null) {
-			if (x3.equals(x5) && x5.equals(x7)) {
-				if (x3.contains("O")) {
-					this.setTitle("Spieler 2 hat gewonnen! Spieler 1 ist an der Reihe");
-					clearButton();
-				}
-				if (x3.contains("X")) {
-					this.setTitle("Spieler 1 hat gewonnen! Spieler 2 ist an der Reihe");
-					clearButton();
-				}
-			}
-		}
-		// else{
-		// System.out.println("Niemand hat gewonnen!");
-		// }
 	}
 
 	public void actionPerformed(ActionEvent ae) {
@@ -244,13 +259,13 @@ public class TICTACTOE extends JFrame implements ActionListener {
 
 		if (ae.getSource() == TICTACTOE.button1) {
 			if (spieler % 2 == 1) {
-				button1.setText("X");
+				button1.setText("O");
 				x1 = button1.getText();
 				gewinnerTest();
 			}
 
 			if (spieler % 2 == 0) {
-				button1.setText("O");
+				button1.setText("X");
 				x1 = button1.getText();
 				gewinnerTest();
 			}
@@ -258,13 +273,13 @@ public class TICTACTOE extends JFrame implements ActionListener {
 
 		if (ae.getSource() == TICTACTOE.button2) {
 			if (spieler % 2 == 1) {
-				button2.setText("X");
+				button2.setText("O");
 				x2 = button2.getText();
 				gewinnerTest();
 			}
 
 			if (spieler % 2 == 0) {
-				button2.setText("O");
+				button2.setText("X");
 				x2 = button2.getText();
 				gewinnerTest();
 			}
@@ -273,26 +288,26 @@ public class TICTACTOE extends JFrame implements ActionListener {
 		if (ae.getSource() == TICTACTOE.button3) {
 
 			if (spieler % 2 == 1) {
-				button3.setText("X");
+				button3.setText("O");
 				x3 = button3.getText();
 				gewinnerTest();
 			}
 
 			if (spieler % 2 == 0) {
-				button3.setText("O");
+				button3.setText("X");
 				x3 = button3.getText();
 				gewinnerTest();
 			}
 		}
 		if (ae.getSource() == TICTACTOE.button4) {
 			if (spieler % 2 == 1) {
-				button4.setText("X");
+				button4.setText("O");
 				x4 = button4.getText();
 				gewinnerTest();
 			}
 
 			if (spieler % 2 == 0) {
-				button4.setText("O");
+				button4.setText("X");
 				x4 = button4.getText();
 				gewinnerTest();
 			}
@@ -300,13 +315,13 @@ public class TICTACTOE extends JFrame implements ActionListener {
 
 		if (ae.getSource() == TICTACTOE.button5) {
 			if (spieler % 2 == 1) {
-				button5.setText("X");
+				button5.setText("O");
 				x5 = button5.getText();
 				gewinnerTest();
 			}
 
 			if (spieler % 2 == 0) {
-				button5.setText("O");
+				button5.setText("X");
 				x5 = button5.getText();
 				gewinnerTest();
 			}
@@ -314,13 +329,13 @@ public class TICTACTOE extends JFrame implements ActionListener {
 
 		if (ae.getSource() == TICTACTOE.button6) {
 			if (spieler % 2 == 1) {
-				button6.setText("X");
+				button6.setText("O");
 				x6 = button6.getText();
 				gewinnerTest();
 			}
 
 			if (spieler % 2 == 0) {
-				button6.setText("O");
+				button6.setText("X");
 				x6 = button6.getText();
 				gewinnerTest();
 			}
@@ -328,13 +343,13 @@ public class TICTACTOE extends JFrame implements ActionListener {
 
 		if (ae.getSource() == TICTACTOE.button7) {
 			if (spieler % 2 == 1) {
-				button7.setText("X");
+				button7.setText("O");
 				x7 = button7.getText();
 				gewinnerTest();
 			}
 
 			if (spieler % 2 == 0) {
-				button7.setText("O");
+				button7.setText("X");
 				x7 = button7.getText();
 				gewinnerTest();
 			}
@@ -342,13 +357,13 @@ public class TICTACTOE extends JFrame implements ActionListener {
 
 		if (ae.getSource() == TICTACTOE.button8) {
 			if (spieler % 2 == 1) {
-				button8.setText("X");
+				button8.setText("O");
 				x8 = button8.getText();
 				gewinnerTest();
 			}
 
 			if (spieler % 2 == 0) {
-				button8.setText("O");
+				button8.setText("X");
 				x8 = button8.getText();
 				gewinnerTest();
 			}
@@ -356,13 +371,13 @@ public class TICTACTOE extends JFrame implements ActionListener {
 
 		if (ae.getSource() == TICTACTOE.button9) {
 			if (spieler % 2 == 1) {
-				button9.setText("X");
+				button9.setText("O");
 				x9 = button9.getText();
 				gewinnerTest();
 			}
 
 			if (spieler % 2 == 0) {
-				button9.setText("O");
+				button9.setText("X");
 				x9 = button9.getText();
 				gewinnerTest();
 			}
